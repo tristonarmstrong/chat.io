829aae9c64205ba808494391dac1e27754de64bc
Triston <triston@Tristons-MacBook-Air.local>
2020-06-10 21:29:51 -0500

    Updates

---
+++ precache-manifest.e1f20b31287b55aee0484dd8fdfa4a7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6028630ea2ded1641b2c06b55c56e902",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95b425e8c9da1bafa4be",
+    "url": "/static/css/main.02747411.chunk.css"
+  },
+  {
+    "revision": "8a35b90e35af96517e97",
+    "url": "/static/js/2.0ea00669.chunk.js"
+  },
+  {
+    "revision": "328cdb9dd54bf8eadab89a968382d928",
+    "url": "/static/js/2.0ea00669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95b425e8c9da1bafa4be",
+    "url": "/static/js/main.78d7a4e1.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
