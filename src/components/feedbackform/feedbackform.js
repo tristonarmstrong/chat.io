ef047d69b1cdbafd5318b7f50e5558e7c9102ee9
Triston <triston@Tristons-MacBook-Air.local>
2020-06-28 18:00:02 -0500

    Update styling

--- src/components/feedbackform/feedbackform.js
+++ src/components/feedbackform/feedbackform.js
@@ -9,28 +9,31 @@ class FeedbackForm extends React.Component{
             .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
             .join("&");
       }
+
+    triggerMessage = (message) => {
+        this.setState({response: true, response_msg: message}, ()=>{
+            setTimeout(() => {
+                this.setState({response: false})
+            }, 5000);
+        })
+    }
     
         /* Here’s the juicy bit for posting the form submission */
-    
-    handleSubmit = e => {
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: this.encode({ "form-name": "feedback", ...this.state })
+    sendFeedback = () => fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: this.encode({ "form-name": "feedback", ...this.state })
+    })
+        .then(() => {
+            this.setState({name:'',email:'',message:'',subject:''}, () => this.triggerMessage('Your feedback is appreciated!'))
         })
-            .then(() => {
-                this.setState({name:'',email:'',message:'',subject:'', response: true, response_msg: 'Your feedback is appreciated!'}, () =>{
-                    setTimeout(() => {
-                        this.setState({response: false})
-                    }, 5000);
-                })
-            })
-            .catch(error => this.setState({response: true, response_msg: error}), ()=>{
-                setTimeout(() => {
-                    this.setState({response: false})
-                }, 5000);
-            });
+        .catch(error => this.triggerMessage(error));
 
+
+    handleSubmit = e => {
+        const {name,email,message,subject} = this.state
+        if(name&&email&&message&&subject) this.sendFeedback()
+        else this.triggerMessage('Please complete the form and try again!')
         e.preventDefault();
     };
     
@@ -38,10 +41,9 @@ class FeedbackForm extends React.Component{
 
     render(){
         const {name, email, message, subject} = this.state
-        ++this.childKey
         return (
             <div className='modal'>
-                {this.state.response && <ClientMessage key={this.childKeyÍ} msg={this.state.response_msg}/>}
+                {this.state.response && <ClientMessage msg={this.state.response_msg}/>}
                 <div className='landing-instructions'>
                     <h1><span>FEEDBACK</span></h1>
                     <p>Please tell me what I can do to improve the app!</p>
